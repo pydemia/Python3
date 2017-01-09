f3d245ee7478359c160fa31770b5e22193e568f7
Youngju Kim <pydemia@gmail.com>
2017-01-09 22:49:13 +0900

    Update myusecase_codes.py

--- misc/myusecase_codes.py
+++ misc/myusecase_codes.py
@@ -1,1 +1,12 @@
+# Useful Tips
+
+### subset rows on condition(it supports groupby object)
+```python
 a.col1 = lambda row: np.where(pd.notnull(a.col2)==True, a.col2, a.col3)
+```
+
+### groupby key columns, subset rows on condition(by values of calculation with multiple columns)
+```python
+data = data.groupby(prm_key).filter(lambda x: mape_array(x['col1'], x['col2']) < 3)
+```
+
